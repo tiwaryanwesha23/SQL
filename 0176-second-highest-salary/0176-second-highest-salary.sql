df522309e1d105e9b381cb08a5cbacad000bc406
ANWESHA TIWARY <86918964+tiwaryanwesha23@users.noreply.github.com>
2023-10-14 17:25:57 +0530

    Time: 427 ms (86.45%), Space: 0B (100.00%) - LeetHub

---
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+select IFNULL(
+(select distinct
+    Salary
+from
+    Employee
+order by
+    Salary desc
+limit
+    1, 1), NULL) SecondHighestSalary;
